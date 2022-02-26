098cbf081d38cdd044509bf63815d8a3dcb4a926
nuriafari <88190164+nuriafari@users.noreply.github.com>
2022-02-26 21:06:17 +0000

    changes

--- PygameDisplay.py
+++ PygameDisplay.py
@@ -5,22 +5,71 @@ Created on Sat Feb 26 12:14:08 2022
 @author: nuria
 
 
-Ideas #todo
-- Add either a disgusting image, or a cute image, to make u be not nearby screen
+
+#todo
+- change so that it does not pop up two times if I am already away (maybe sleep for 1sec? lets try)
 """
 from __future__ import print_function
-from WebcamVideoStream import WebcamVideoStream
 from threading import Thread
 import pygame
-import os
+import datetime
 import time
+
+import os
+import json
 import cv2 as cv
 import argparse
 import ctypes  # An included library with Python install.
 
 
-#todo - remove random
-import random
+stream = cv.VideoCapture(0, cv.CAP_DSHOW)
+pygame.init()
+screen_width, screen_height = 500, 500
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+
+font_30 = pygame.font.SysFont("calibri", 30)
+font_18 = pygame.font.SysFont("calibri", 18) 
+font_16 = pygame.font.SysFont("calibri", 16)
+
+
+filepath = os.path.dirname(__file__)
+
+
+class button():
+    def __init__(self, color, x,y,width,height, text='', font=font_30):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.font = font
+
+    def draw(self,screen):
+        #Call this method to draw the button on the screen            
+        pygame.draw.rect(screen, self.color, (self.x,self.y,self.width,self.height),0,5)
+        
+        if self.text != '':
+            text = self.font.render(self.text, 1, (255,255,255))
+            screen.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+    
+    def isOver(self, pos):
+        #Pos is the mouse position or a tuple of (x,y) coordinates
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True
+            
+        return False
+
+def change_button_color(button_key, pos, default_color=(0,0,0), isOver_color=(0,0,255)):
+    if button_key.isOver(pos):
+        button_key.color = isOver_color
+    else:
+        button_key.color = default_color
+    button_key.draw(screen)
+    pygame.display.update()
+
 
 def detectWidth(frame):
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -48,16 +97,28 @@ def Mbox(title, text, style):
     return ctypes.windll.user32.MessageBoxW(0, text, title, style)
 
 
-def pygame_loop():
-    pygame.init()
-    screen_width = 700
-    screen_height = 500
-    screen = pygame.display.set_mode((screen_width, screen_height))
+def write_json(average_distance, num_popups, time_opened):
+    YEAR        = datetime.date.today().year
+    MONTH       = datetime.date.today().month
+    DATE        = datetime.date.today().day
+    HOUR        = datetime.datetime.now().hour
+    MINUTE      = datetime.datetime.now().minute
+    SECONDS     = datetime.datetime.now().second
 
-    pygame_icon_png = "Eyes.png"    #icon of pygame. Must be 32x32 pixels
 
-    font_30 = pygame.font.SysFont("calibri", 30)
-    font_18 = pygame.font.SysFont("calibri", 30)
+    json_dict = {
+        "h_{}_m_{}".format(HOUR,MINUTE): {
+            "average_distance" : average_distance,
+            "num_popups": num_popups,
+            "time_opened": time_opened
+            }
+        }
+        
+    json_file = open(os.path.join(filepath,"dict.json"),"w")
+    json.dump(json_dict, json_file)
+    json_file.close()
+
+def pygame_intro():
 
     def write_message(message, color = (0,0,0), rectangle=[0,0], font=font_18, update = True, centered = False):
         mesg = font.render(message, True, color)
@@ -67,26 +128,52 @@ def pygame_loop():
         screen.blit(mesg, rectangle)
         if update:
             pygame.display.update()
-
-    def RedScreen():
-        screen.fill((200,0,0))
-        write_message("you are too close to the screen!", color = (255,255,255), rectangle=(screen_width/2, screen_height/2), centered=True, font=font_30)
-
-    Open = True
-    while Open: 
-        RedScreen()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                Open = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
+    
+    def Initial_Screen():
+        screen.fill((255,255,255))
+        write_message("Welcome to Screen distance tracker!", rectangle=(screen_width/2, 20), centered=True, font=font_18)
+        write_message("While open, this app will warn you if you are too close to the screen", rectangle=(screen_width/2, 50), centered=True, font=font_16)
+        write_message("This link will show you what will happen if you do this too often", rectangle=(screen_width/2, 80), centered=True, font=font_16)
+        write_message("Do you want to start the app?", rectangle=(screen_width/2, 110), centered=True, font=font_16)
+        
+        button_off = button((255,0,0),220,220,50,50,text="OFF")
+        button_off.draw(screen)
+        pygame.display.update()
+        
+        Open = True
+        while Open:
+            for event in pygame.event.get():
+                
+                if event.type == pygame.MOUSEMOTION:
+                    pos = pygame.mouse.get_pos()
+                    if button_off.isOver(pos):
+                        if button_off.color == (255,0,0):
+                            button_off = button((100,0,0),220,220,50,50,text="OFF")
+                            button_off.draw(screen)
+                            pygame.display.update()
+                    elif button_off.color == (100,0,0):
+                        button_off = button((255,0,0),220,220,50,50,text="OFF")
+                        button_off.draw(screen)
+                        pygame.display.update()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if button_off.color == (100,0,0):
+                        button_off = button((0,255,0),220,220,50,50,text="ON")
+                        button_off.draw(screen)
+                        pygame.display.update()
+                        time.sleep(1)
+                        Open = False
+                
+                if event.type == pygame.QUIT:
                     Open = False
-            
-    pygame.quit()    
-
-
-
-
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        Open = False
+                    if event.key == pygame.K_q:
+                        Open = False
+                        
+    
+    Initial_Screen()
+                        
 parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
 parser.add_argument('--face_cascade', help='Path to face cascade.', default= cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default= cv.data.haarcascades + 'haarcascade_eye.xml')
@@ -105,35 +192,75 @@ if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
 camera_device = args.camera
 
 
-vs = WebcamVideoStream(src=0).start()
-stream = cv.VideoCapture(0, cv.CAP_DSHOW)
-pygame.init()
-screen = pygame.display.set_mode((500, 500))
-clock = pygame.time.Clock()
 def updateGUI():
     clock.tick(60)
+    
+
+pygame_intro()
+button_off = button((0,255,0),220,220,50,50,text="ON")
+button_off.draw(screen)
+
 Thread(target=updateGUI, args=()).start()
 
+
+start = time.time()
+distance_list=[]
+num_popups = 0
+
+Close = False
 toggle = False
 while True:
+    start_close_screen = time.time()
+    still_close_screen = time.time()
+    
     ret, frame = stream.read()
     width = detectWidth(frame)
-    print(width)
+    distance_list.append(width)
+
     if width > 180:
-        pygame.init()
-        screen.fill((200,0,0))
-        Mbox('Your title', 'Your text', 0)
-        warning = True
-    else:
-        screen.fill((255, 255, 255))
-        warning = False
-
+        Mbox('Get away!', 'You are too close to the screen', 0)
+        num_popups +=1
+        startloop = time.time()
+    
+    
+    for event in pygame.event.get():
+        
+        if event.type == pygame.MOUSEMOTION:
+            pos = pygame.mouse.get_pos()
+            if button_off.isOver(pos):
+                if button_off.color == (0,255,0):
+                    button_off = button((0,100,0),220,220,50,50,text="ON")
+                    button_off.draw(screen)
+                    pygame.display.update()
+            elif button_off.color == (0,100,0):
+                button_off = button((0,255,0),220,220,50,50,text="ON")
+                button_off.draw(screen)
+                pygame.display.update()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button_off.color == (0,100,0):
+                button_off = button((255,0,0),220,220,50,50,text="OFF")
+                print("off")
+                button_off.draw(screen)
+                pygame.display.update()
+                Close = True
+        
+        if event.type == pygame.QUIT:
+            Close = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                Close = True
+            if event.key == pygame.K_q:
+                Close = True
+    
     pygame.display.flip()
-
-
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    
+    if (cv.waitKey(1) & 0xFF == ord('q')) or Close == True:
         pygame.quit()
         break
 
+average_distance = 0
+end = time.time()
+time_opened = end - start
+write_json(average_distance, num_popups, time_opened)
 stream.release()
 cv.destroyAllWindows()
